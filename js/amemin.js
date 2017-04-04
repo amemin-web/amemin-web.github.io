0c2a9e841d139bfe2ae6c50493cb6b7eaf0dd7af
Takuma Yonezu <yoneztyty@gmail.com>
2017-04-04 03:25:34 +0000

    chart

--- js/amemin.js
+++ js/amemin.js
@@ -1,62 +1,35 @@
 $(function () {
-  var colors = new Array(
-    [62,35,255],
-    [60,255,60],
-    [255,35,98],
-    [45,175,230],
-    [255,0,255],
-    [255,128,0]);
-
-  var step = 0;
-  //color table indices for:
-  // current color left
-  // next color left
-  // current color right
-  // next color right
-  var colorIndices = [0,1,2,3];
-
-  //transition speed
-  var gradientSpeed = 0.002;
-
-  function updateGradient()
-  {
-
-    if ( $===undefined ) return;
-
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
-
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb("+r1+","+g1+","+b1+")";
-
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb("+r2+","+g2+","+b2+")";
-
-   $('#gradient').css({
-     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-
-    step += gradientSpeed;
-    if ( step >= 1 )
-    {
-      step %= 1;
-      colorIndices[0] = colorIndices[1];
-      colorIndices[2] = colorIndices[3];
-
-      //pick two new target color indices
-      //do not pick the same as the current one
-      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-
+  var ctx = document.getElementById("myChart-inst").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ["vocal：27%", "Guitar：27%", "Bass：19%", "Drumms：17%", "Key & Synth：10%"],
+      datasets: [{
+        backgroundColor: [
+          "#2ecc71",
+          "#3498db",
+          "#95a5a6",
+          "#9b59b6",
+          "#f1c40f",
+          "#e74c3c",
+          "#34495e"
+        ],
+        data: [27, 27, 19, 17, 10]
+      }]
     }
-  }
-
-  setInterval(updateGradient,10);
+});
+var ctx = document.getElementById("myChart-fm").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'pie',
+data: {
+  labels: ["Girls", "Boys"],
+  datasets: [{
+    backgroundColor: [
+      "#e74c3c",
+      "#3498db",
+    ],
+    data: [42,58 ]
+  }]
+}
+});
 });
